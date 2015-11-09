fc316a1ad90794a3d65c6ac64b6288d43291fdcf
Lefteris Nikoltsios <lefteris.nikoltsios@gmail.com>
2015-11-09 14:55:18 +0200

    Initial commit.
    Add project structure from:
     https://github.com/pydanny/cookiecutter-djangopackage

---
+++ README.rst
@@ -0,0 +1,21 @@
+================================
+Open source django short url app
+================================
+
+.. image:: https://travis-ci.org/lefterisnik/django-shorturl.png?branch=master
+    :target: https://travis-ci.org/lefterisnik/django-shorturl
+
+A django short URL app.
+
+Quickstart
+----------
+
+Install Open source django short url app.::
+
+    pip install django-shorturl
+
+Cookiecutter Tools Used in Making This Package
+----------------------------------------------
+
+*  cookiecutter
+*  cookiecutter-djangopackage
---
+++ setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+import shorturl
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = shorturl.__version__
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload')
+    sys.exit()
+
+if sys.argv[-1] == 'tag':
+    print("Tagging the version on github:")
+    os.system("git tag -a %s -m 'version %s'" % (version, version))
+    os.system("git push --tags")
+    sys.exit()
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+setup(
+    name='django-shorturl',
+    packages=['shorturl'],
+    version=version,
+    description='A django short URL app.',
+    long_description=readme + '\n\n' + history,
+    author='Lefteris Nikoltsios',
+    author_email='lefteris.nikoltsios@gmail.com',
+    url='https://github.com/lefterisnik/django-shorturl',
+    include_package_data=True,
+    license="BSD",
+    zip_safe=False,
+    keywords='django-shorturl',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    install_requires=[
+    ],
+)
